7e5463752f421d1fd263c2f57f42d737b50f2fdd
Susmoy-Das02 <methundas269@gmail.com>
2022-05-25 03:18:46 +0600

    user add in database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
+const { MongoClient, ServerApiVersion, MongoRuntimeError, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -12,21 +12,45 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        await client.connect();
-        const itemCollection = client.db('circuit_city').collection('items');
-        app.get('/items', async(req, res) =>{
-            const query = {};
-            const cursor = itemCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
-        })
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db('circuit_city').collection('items');
+    const userCollection = client.db('circuit_city').collection('users');
 
-    }
-    finally{
+    app.get('/item', async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
 
-    }
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+
+    })
+
+
+
+    app.get('/item/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemCollection.findOne(query);
+      res.send(item);
+    })
+
+  }
+  finally {
+
+  }
 
 }
 run().catch(console.dir);
